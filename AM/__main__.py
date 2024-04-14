80f0de11d910166bd8f673292b2eb4db8fd46606
HerokuTaccc <154675149+HerokuTaccc@users.noreply.github.com>
2024-04-14 17:20:12 +0530

    Create __main__.py

---
+++ AM/__main__.py
@@ -0,0 +1,21 @@
+import asyncio
+import importlib
+from pyrogram import idle
+from AM import AM
+from AM.modules import ALL_MODULES
+
+ 
+
+loop = asyncio.get_event_loop()
+
+
+async def Nex_boot():
+    for all_module in ALL_MODULES:
+        importlib.import_module("AM.modules." + all_module)
+    print("""𝐘𝐨𝐮𝐫 𝐛𝐨𝐭 𝐡𝐚𝐬 𝐬𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 𝐬𝐭𝐚𝐫𝐭𝐞𝐝 𝐚𝐧𝐝 𝐢𝐬 𝐫𝐞𝐚𝐝𝐲 𝐭𝐨 𝐚𝐬𝐬𝐢𝐬𝐭""")
+    await idle()
+    print("»» ɢᴏᴏᴅ ʙʏᴇ ! sᴛᴏᴘᴘɪɴɢ ʙᴏᴛ.")
+
+
+if __name__ == "__main__":
+    loop.run_until_complete(Nex_boot())
