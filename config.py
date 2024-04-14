fee7ae399402e359068aa422da5efa60741f070d
HerokuTaccc <154675149+HerokuTaccc@users.noreply.github.com>
2024-04-14 17:24:06 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,62 @@
+import os
+from os import getenv
+
+
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_USERNAME = getenv("BOT_USERNAME", "oshio")
+BOT_TOKEN = getenv("BOT_TOKEN", "6920116730:AAEPKgYMyh6OX558T11i-6lM1yVe74SpSlw")
+OWNER_ID = int(getenv("OWNER_ID", "6590287973"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6590287973 6180567980 6180567980 6180567980 6180567980 6180567980 6297223639 6594200317 6070538973").split()))
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://vinamratiwari579:m6YDRYH8HbwuEqxt@cluster0.x7ac1wt.mongodb.net/?retryWrites=true&w=majority")
+SESSION_STRING = getenv("SESSION_STRING","")
+CHANNEL = getenv("CHANNEL", "Logs_Gban")
+SUPPORT = getenv("SUPPORT", "https://t.me/naruto_support1")
+OWNER_USERNAME = getenv("OWNER_USERNAME","New_AMBOT")
+PING_PIC = ["https://telegra.ph/file/7c25ef427c9f3cded5577.jpg","https://telegra.ph/file/625d235cc0a22fb8525b5.jpg","https://telegra.ph/file/1c62254d59baf7f968ba7.jpg","https://telegra.ph/file/7a0553bd4664486ab3008.jpg","https://telegra.ph/file/7b4dfa606e6f23961d30e.jpg", "https://telegra.ph/file/2773dec98d87b8562618c.jpg", "https://telegra.ph/file/80353d02e0368b71d2666.jpg","https://telegra.ph/file/6e5331dc4bef87464ea1c.jpg","https://telegra.ph/file/199a2e44cb8e77bb21b34.jpg","https://telegra.ph/file/8371bcd8952d089f9ec05.jpg","https://telegra.ph/file/f970e559dd1bb96fced1a.jpg","https://telegra.ph/file/59a305f8ce0c4e85949cc.jpg"]
+SuperFban = getenv("SuperFban","") #AMBOT V1
+SuperFban1 = getenv("SuperFban1","") #AMBOT V2
+SuperFban2 = getenv("SuperFban2","") #Sohial
+SuperFban3 = getenv("SuperFban3","") #Vicy
+SuperFban4 = getenv("SuperFban4","") #Sagar Fed 
+SuperFban5 = getenv("SuperFban5","")#Durgesh
+SuperFban6 = getenv("SuperFban6","") #raistarbot
+SuperFban7 = getenv("SuperFban7","")#Naruto fed
+SuperFban8 = getenv("SuperFban8","") #Madara
+SuperFban9 = getenv("SuperFban9","")
+SuperFban10 = getenv("SuperFban10","")
+SuperFban11 = getenv("SuperFban11","")
+SuperFban12 = getenv("SuperFban12","")
+FBAN_LOGS1 =  getenv("FBAN_LOGS1","-1002143461069")
+FBAN_LOGS =  getenv("FBAN_LOGS","-1001997062793")
+F1 = getenv("F1","6c869a9b-5277-4da4-ad14-4fdbde69f517")
+F2 = getenv("F2","5bbff394-1877-4811-9345-da14fe5431c7")
+F3 = getenv("F3","189a2651-4a70-448e-a4bb-c907b7b7e787")
+F4 = getenv("F4","1a61cca5-d446-4aaf-80d9-8f91ea1d8b63")
+F5 = getenv("F5","8e162144-d4e2-4f2c-9ab9-0ddeccb168d9")
+F6 = getenv("F6","71eb5c53-e054-4840-a708-fa0d3c374bd5")
+F7 = getenv("F7","17ed414c-b434-4c74-a408-9ce6ed77e8a6")
+F8 = getenv("F8","befab9d9-bb33-448e-ace3-7a233ee8f6a0")
+F9 = getenv("F9","a560f4b4-5607-4fb9-9947-1189ac621dee")
+F10 = getenv("F10","1f228c7a-ca8e-4e6b-bcad-338ab3653a0f")
+F11 = getenv("F11","a1c843b9-3ea2-4e66-aaf2-3df45ed8489f")
+F12 = getenv("F12","b20f0c74-fe62-46c2-a099-90a56e10f657")
+F13 = getenv("F13","de7d6c87-6217-4b89-bc50-923314629673")
+F14 = getenv("F14","4228c7a-ca8e-4e6b-bcad-338ab3653a0f")
+F15 = getenv("F15","dbd28b3f-5614-458b-9ca2-d1c2083dbb81")
+F16 = getenv("F16","c787613b-5e94-41b9-9d9b-1c7d383dd22d")
+F17 = getenv("F17","61c56607-ea7d-4893-9d4f-770cd63d7626")
+F18 = getenv("F18","7e735aab-366f-4a52-b208-9473396e1464")
+F19 = getenv("F19","84388add-01c8-4bfa-bee0-3d0b9e42298f")
+F20 = getenv("F20","56ff020a-b81d-4651-a947-bf60369a3fcb")
+FBAN = getenv("FBAN","ꜱᴜᴘᴇʀ ʙᴀɴɴᴇᴅ ʙʏ : ")
+UNFBAN = getenv("UNFBAN","ꜱᴜᴘᴇʀ ᴜɴʙᴀɴɴᴇᴅ ʙʏ : " )  
+APPEND = """
+ᴜɴꜰʙᴀɴ ʜᴇʀᴇ : 
+@AM_YTSupport  
+@SpicyEmpireSupport
+@Dream_appeal
+
+ᴘᴏᴡᴇʀ ʙʏ : @SpicyEmpire
+"""
+Doneapped = "ᴘᴏᴡᴇʀ ʙʏ : @SpicyEmpire"
